ef7f8edb21c673e911aad5d908dc67ebce632d49
Elliot Rask <elliot.rask@swace.se>
2025-02-16 14:37:09 +0100

    Move todo

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,9 +3,6 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
