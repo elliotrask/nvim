a90bf85dd68a59f130fb307f5917ed32590b2266
Elliot Rask <elliot.rask@swace.se>
2025-02-16 14:35:32 +0100

    Change harpoon config

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -11,29 +11,40 @@ return {
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
-    end)
-    vim.keymap.set('n', '<C-e>', function()
+    end, { desc = '[A]dd to Harpoon' })
+    -- <A-g> is not a valid keybinding, so we use ¸
+    vim.keymap.set('n', '¸', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', '<C-h>', function()
-      harpoon:list():select(1)
-    end)
-    vim.keymap.set('n', '<C-t>', function()
-      harpoon:list():select(2)
-    end)
-    vim.keymap.set('n', '<C-n>', function()
-      harpoon:list():select(3)
-    end)
-    vim.keymap.set('n', '<C-s>', function()
-      harpoon:list():select(4)
+    -- Clear Harpoon list
+    -- <A-x> is not a valid keybinding, so we use ≈
+    vim.keymap.set('n', '≈', function()
+      harpoon:list():clear()
     end)
 
+    -- <A-n> is not a valid keybinding, so we use ‘
+    vim.keymap.set('n', '‘', function()
+      harpoon:list():select(1)
+    end)
+    -- <A-e> is not a valid keybinding, so we use é
+    vim.keymap.set('n', 'é', function()
+      harpoon:list():select(2)
+    end)
+    -- <A-i> is not a valid keybinding, so we use ı
+    vim.keymap.set('n', 'ı', function()
+      harpoon:list():select(3)
+    end)
+    -- <A-o> is not a valid keybinding, so we use œ
+    vim.keymap.set('n', 'œ', function()
+      harpoon:list():select(4)
+    end)
+    --
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '<C-S-P>', function()
+    vim.keymap.set('n', '›', function()
       harpoon:list():prev()
     end)
-    vim.keymap.set('n', '<C-S-N>', function()
+    vim.keymap.set('n', '√', function()
       harpoon:list():next()
     end)
   end,
